6a3b5b2e0809abbcf601691aa585330e3c98fcce
bastianleicht <mail@bastianleicht.de>
2021-08-23 21:24:41 +0200

    Software: Initial Commit

---
+++ src/Software.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Venocix\HosterAPI;
+
+use GuzzleHttp\Exception\GuzzleException;
+
+class Software
+{
+    private $HosterAPI;
+
+    public function __construct(HosterAPI $HosterAPI)
+    {
+        $this->HosterAPI = $HosterAPI;
+    }
+
+    /**
+     * Returns a list of currently available software
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function list()
+    {
+        return $this->HosterAPI->get('software/list');
+    }
+
+    /**
+     * Installs the chosen software
+     * @param string $server_id_or_ip
+     * @param string $package
+     * @param string $password
+     * @param int $port
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function install(string $server_id_or_ip, string $package, string $password, int $port)
+    {
+        return $this->HosterAPI->post('software/install', [
+            'sid|ip' => $server_id_or_ip,
+            'package' => $package,
+            'password' => $password,
+            'port' => $port,
+        ]);
+    }
+
+    /**
+     * Uninstalls the chosen software
+     * @param string $server_id_or_ip
+     * @param string $package
+     * @param string $password
+     * @param int $port
+     * @return array|string
+     * @throws GuzzleException
+     */
+    public function uninstall(string $server_id_or_ip, string $package, string $password, int $port)
+    {
+        return $this->HosterAPI->post('software/uninstall', [
+            'sid|ip' => $server_id_or_ip,
+            'package' => $package,
+            'password' => $password,
+            'port' => $port,
+        ]);
+    }
+
+}
